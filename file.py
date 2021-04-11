f56997e6810cf1511fb9b7c99212865b2b22482a
Cyber-Coding-Scripts <82296144+Cyber-Coding-Scripts@users.noreply.github.com>
2021-04-10 21:16:34 -0400

    Create file.py

---
+++ file.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import filedialog
+import os
+
+def openFile():
+    filepath = filedialog.askopenfilename(initialdir="/",
+                                          title="Open file okay?",
+                                          filetypes= (("text files","*.txt"),
+                                          ("all files","*.*")))
+    os.startfile(filepath)
+
+window = Tk()
+button = Button(text="Open",command=openFile)
+button.pack()
+window.mainloop()
